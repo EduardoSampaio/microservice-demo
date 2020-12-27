e57610fcf11c4d78fe5cf777bc53d9cb7065bef7
Eduardo Sampaio <edwardviana@gmail.com>
2020-12-27 12:03:53 -0300

    first commit

---
+++ hr-payroll/src/main/java/com/microservice/payroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.microservice.payroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.microservice.payroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(long workerId, int days) {
+		return new Payment("Bob",200.0, days);
+	}
+}
